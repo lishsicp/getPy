69d8aded3ea92528b0020a73458215ce42329687
Yaroslav Lobur <35396993+lishsicp@users.noreply.github.com>
2021-04-21 18:03:46 +0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,89 +1,102 @@
 # write your code here
 
+
 win_count = 0
 winner = ''
-def impossible(cells):
-    player_x = [i for i in cells if i == 'X']
-    player_o = [i for i in cells if i == 'O']
-    if abs(len(player_x) - len(player_o)) > 1:
-        return True
+cells = []
+
+current_player = 'O'
+cells = '_________'
+cells = cells.replace('_', ' ').upper()
+cells = list(cells)
+
+def board_display():
+    print("---------")
+    print("|", cells[0], cells[1], cells[2], "|")
+    print("|", cells[3], cells[4], cells[5], "|")
+    print("|", cells[6], cells[7], cells[8], "|")
+    print("---------")
+    return
+
+board_display()
+
+# def no_winner(cells):
     
+            
 def diagonal_win(cells):
     global win_count
-    if cells[0] == cells[4] == cells[8]:
+    global winner
+    if cells[0] == cells[4] == cells[8] != ' ':
         win_count += 1
-        return cells[0] + ' wins'
-    if cells[2] == cells[4] == cells[6]:
+        winner = cells[0] + ' wins'
+        return True
+    elif cells[2] == cells[4] == cells[6] != ' ':
         win_count += 1
-        return cells[2] + ' wins'
-            
+        winner = cells[2] + ' wins'
+        return True
+
 def horizontal(cells):
     global win_count
     global winner
     for i in range(0, 7, 3):
-        if cells[i] == cells[i + 1] == cells[i + 2]:
+        if cells[i] == cells[i + 1] == cells[i + 2] != ' ':
             win_count += 1
             winner = cells[i]
             return True
-                
+
 def vertical(cells):
     global win_count
     global winner
     for i in range(0, 3):
-        if cells[i] == cells[i + 3] == cells[i + 6]:
+        if cells[i] == cells[i + 3] == cells[i + 6] != ' ':
             win_count += 1
             winner = cells[i]
             return True
-                
-def win_impossible(cells):
-    if cells[0] == cells[3] == cells[6] and cells[1] == cells[4] == cells[7]:
-        return 1
-    elif cells[1] == cells[4] == cells[7] and cells[2] == cells[5] == cells[8]:
-        return 1
-    elif cells[0] == cells[3] == cells[6] and cells[2] == cells[5] == cells[8]:
-        return 1
-    elif cells[0] == cells[1] == cells[2] and cells[3] == cells[4] == cells[5]:
-        return 1
-    elif cells[0] == cells[1] == cells[2] and cells[6] == cells[7] == cells[8]:
-        return 1
-    elif cells[3] == cells[4] == cells[5] and cells[6] == cells[7] == cells[8]:
-        return 1
 
-        
-def game_not_finished(cells):
-    for i in range(len(cells)):
-        if cells[i] == "_":
-            return 'Game not finished'
-            
-cells = input('Enter cells: ')
+def check_for_index(a, b):
+    i = a - 1
+    j = b + 2
+    num = (i * 3 + j) - 3
+    return num
 
-print('---------')
-print('| ' + cells[0] + ' ' + cells[1] + ' ' + cells[2] + ' |')
-print('| ' + cells[3] + ' ' + cells[4] + ' ' + cells[5] + ' |')
-print('| ' + cells[6] + ' ' + cells[7] + ' ' + cells[8] + ' |')
-print('---------')
+while True:
+    if win_count == 1:
+        break
+    coordinates = input('Enter the coordinates: ').split()
+    index = check_for_index(int(coordinates[0]), int(coordinates[1]))
+    if coordinates[0].isalpha() or coordinates[1].isalpha():
+        print('You should enter numbers!')
+    elif int(coordinates[0]) < 1 or int(coordinates[0]) > 3 or int(coordinates[1]) < 1 or int(coordinates[1]) > 3:
+        print("Coordinates should be from 1 to 3!")
+    elif cells[index] == 'X' or cells[index] == 'O':
+        print("This cell is occupied! Choose another one!")
+    else:
+        # cells[index] = current_player
+        if current_player == 'O':
+            current_player = 'X'
+            cells[index] = current_player
+        elif current_player == 'X':
+            current_player = 'O'
+            cells[index] = current_player
+        board_display()
+        horizontal(cells)
+        vertical(cells)
+        diagonal_win(cells)
+        count = 0
+        for i in range(len(cells)):
+            if cells[i] == ' ':
+                count += 1
+        if count == 0:
+            print('Draw')
+            break
+# no_winner(cells)
 
-cells = cells.replace('_', ' ')
-current_player = 'X'
-coordinates = input('Enter the coordinates: ').split
-coo
-def insert_in_board(x):
+if win_count == 1:
+    if horizontal(cells):
+        print(winner + ' wins')
+    elif vertical(cells):
+        print(winner + ' wins')
+    elif diagonal_win(cells):
+        print(winner)
     
 
-
-
-horizontal(cells)
-vertical(cells)
-
-if impossible(cells) or win_impossible(cells) == 1:
-    print("Impossible")
-elif horizontal(cells):
-    print(winner + ' wins')
-elif vertical(cells):
-    print(winner + ' wins')
-elif diagonal_win(cells):
-    print(diagonal_win(cells))
-elif game_not_finished(cells):
-    print(game_not_finished(cells))
-else:
-    print("Draw")        
